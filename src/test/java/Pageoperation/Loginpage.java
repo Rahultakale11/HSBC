7d5c4480edf9cfeb7d1a948ac5ef1c056de8a0b4
rahultakale11 <takalerahul51@gmail.com>
2020-01-27 22:49:58 +0530

    First ranjit commit

---
+++ src/test/java/Pageoperation/Loginpage.java
@@ -0,0 +1,55 @@
+package Pageoperation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import configration.config;
+
+public class Loginpage {
+
+	public Loginpage(WebDriver driver)
+	{
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(how=How.XPATH,using="//input[@placeholder='Username']")
+	WebElement username;
+	public void username(String name)
+    {
+       username.sendKeys(name);
+	}
+	 @FindBy(how=How.XPATH,using="//input[@placeholder='Password']")
+	 WebElement password;
+	 public void password(String pass)
+	 {
+		 password.sendKeys(pass);
+	 }
+     
+	 @FindBy(how=How.XPATH, using="//input[@value='Login']")
+	 WebElement loginclick;
+	 public void click()
+	 {
+		 loginclick.click();
+	 }
+	 
+	 @FindBy(how=How.XPATH,using="//a[text()='Pipeline']")
+		WebElement pipeline;
+		public void clickpipline()
+		{
+			config.driver.switchTo().frame("mainpanel");
+			pipeline.click();
+		}
+		////i[@class='fa fa-sign-out icon-2x']
+		
+		@FindBy(how=How.XPATH,using="//a[@href='https://www.crmpro.com/index.cfm?logout=1']//i")
+		WebElement checklogout;
+		public void checklogin()
+		{
+			System.out.println("hello dharmu");
+			config.driver.switchTo().frame("mainpanel");
+			checklogout.click();
+		}
+}
---
+++ target/cucumber/report.js
@@ -0,0 +1,162 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("myfeature.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Login browser",
+  "description": "",
+  "id": "login-browser",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": "Verification of reset button with numbers of credential",
+  "description": "",
+  "id": "login-browser;verification-of-reset-button-with-numbers-of-credential",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 2,
+      "name": "@SmokeTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 4,
+  "name": ": Open the firefox",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": ": Enter the Username \"\u003cUsername\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": ": Enter Password \"\u003cPassword\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": ": Click login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": ": check validation",
+  "keyword": "And "
+});
+formatter.examples({
+  "line": 9,
+  "name": "",
+  "description": "",
+  "id": "login-browser;verification-of-reset-button-with-numbers-of-credential;",
+  "rows": [
+    {
+      "cells": [
+        "Username",
+        "Password"
+      ],
+      "line": 10,
+      "id": "login-browser;verification-of-reset-button-with-numbers-of-credential;;1"
+    },
+    {
+      "cells": [
+        "ranjitk",
+        "ranjit7001"
+      ],
+      "line": 11,
+      "id": "login-browser;verification-of-reset-button-with-numbers-of-credential;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "Verification of reset button with numbers of credential",
+  "description": "",
+  "id": "login-browser;verification-of-reset-button-with-numbers-of-credential;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 2,
+      "name": "@SmokeTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 4,
+  "name": ": Open the firefox",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": ": Enter the Username \"ranjitk\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": ": Enter Password \"ranjit7001\"",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": ": Click login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": ": check validation",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "step.open_the_firefox()"
+});
+formatter.result({
+  "duration": 21094995825,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "ranjitk",
+      "offset": 22
+    }
+  ],
+  "location": "step.Enter_the_Username(String)"
+});
+formatter.result({
+  "duration": 2162917777,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "ranjit7001",
+      "offset": 18
+    }
+  ],
+  "location": "step.enter_pass(String)"
+});
+formatter.result({
+  "duration": 16394931166,
+  "status": "passed"
+});
+formatter.match({
+  "location": "step.click_login_button()"
+});
+formatter.result({
+  "duration": 60374,
+  "status": "passed"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
